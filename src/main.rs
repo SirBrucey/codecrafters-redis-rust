7e0925ec0fd1e53ae8de36b5595b8c452e1b05db
Stuart Bruce <programming@brucey.me.uk>
2024-10-16 19:20:03 +0100

    Handle concurrent clients
    Rewrite using tokio.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::io;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buf = [0; 512];
-                loop {
-                    let bytes = stream.read(&mut buf).unwrap();
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            process(socket).await
+        });
+    }
+}
 
-                    if bytes == 0 {
-                        break;
-                    }
-
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+async fn process(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    loop {
+        stream.readable().await.unwrap();
+        match stream.try_read(&mut buf) {
+            Ok(0) => break,
+            Ok(_n) => {
+                stream.write_all(b"+PONG\r\n").await.unwrap()
+            },
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
+            Err(e) => panic!("{}", e)
         }
     }
 }
